12509a60f6541a92413c9714a7e219f4e4abe946
huangchen <546958900@qq.com>
2015-11-07 01:27:34 +0800

    version 2

---
+++ src/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "stockmanger"
+
+func main() {
+	/*  stockmgr := stockmanger.NewStockMgr();
+	    if err :=stockmgr.start() err != nil {
+	    	fmt.Println("start error");
+	    }
+	*/
+	fmt.Println("start success")
+
+}
